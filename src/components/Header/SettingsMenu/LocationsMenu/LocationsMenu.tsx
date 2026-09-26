4af2acc5aa5b54b5ac270cf6971aa535895cbd55
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-26 15:46:46 +0300

    test old

--- src/components/Header/SettingsMenu/LocationsMenu/LocationsMenu.tsx
+++ src/components/Header/SettingsMenu/LocationsMenu/LocationsMenu.tsx
@@ -11,8 +11,10 @@ export default function LocationsMenu({ closeLocationsMenu }: LocationsMenuProps
   const { locations, setLocation, pinLocation, saveLocation, removeLocation } = useLocations();
 
   return (
-    <div className="locations-menu">
-      <div className="locations-menu__content">
+    <>
+      <div className="locations-menu__backdrop" />
+
+      <div className="locations-menu">
         <header className="locations-menu__header">
           <h2 className="locations-menu__title">Locations Menu</h2>
 
@@ -93,6 +95,6 @@ export default function LocationsMenu({ closeLocationsMenu }: LocationsMenuProps
           </ul>
         </div>
       </div>
-    </div>
+    </>
   );
 }
